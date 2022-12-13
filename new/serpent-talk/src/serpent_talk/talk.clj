0aba8914409720bb29f902ac3509f05a03a0a576
sudo.park <sudo.park@kakao.com>
2022-12-14 00:18:47 +0900

    lein으로 프로젝트 생성

---
+++ new/serpent-talk/src/serpent_talk/talk.clj
@@ -0,0 +1,8 @@
+(ns serpent-talk.talk)
+;; 해당 파일의 이름공간 -> namespace는 프로젝트이름.파일
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
---
+++ new/serpent-talk/test/serpent_talk/talk_test.clj
@@ -0,0 +1,12 @@
+(ns serpent-talk.talk-test
+  (:require [clojure.test :refer :all]
+            [serpent-talk.talk :refer :all]))
+;; 이름공간이 테스트파일로 바뀌었고
+;; closure.test 라이브러리를 :refer :all로 불러와서 앞에 prefix 안붙이고도 쓸 수 있음
+;; 그리고 테스트하려는 대상도 불러옴
+
+(deftest a-test                                             ;; deftest는 테스트 함수를 정의하고
+  (testing "FIXME, I fail."                                 ;; testing은 deftest에서 사용되며 무엇을 테스트 하는지에 대해 설명한다
+    (is (= 1 1))))                                          ;; is는 실제 테스트를 하는 단언문(assertion)을 제공한다.
+
+
