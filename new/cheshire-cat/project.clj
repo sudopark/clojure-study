5a90c7a6575d3bcac8c094e80a69f4217d5621e6
sudo.park <sudo.park@kakao.com>
2022-12-16 00:54:39 +0900

    ring-json 사용하여 응답 json 으로 바꾸기

--- new/cheshire-cat/project.clj
+++ new/cheshire-cat/project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [cheshire "5.11.0"]]
+                 [ring/ring-json "0.5.1"]]
   ;; lein-ring 플러그인은 웹 서버를 시작하는 것과 같은 일반적인 링 작업을 자동화 한다.
   :plugins [[lein-ring "0.12.5"]]
   ;; :ring :handler 키는 웹 서버를 시작할 때 어디서 app 라우트를 찾아야 하는지 알려준다.
--- new/cheshire-cat/src/cheshire_cat/handler.clj
+++ new/cheshire-cat/src/cheshire_cat/handler.clj
@@ -2,7 +2,8 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [cheshire.core :as json]))
+            [ring.middleware.json :as ring-json]
+            [ring.util.response :as rr]))
 
 ;; defroutes는 app-routes라는 HTTP 라우트를 만든다. -> 클라에서 요청하는 URL의 경로를 처리함
 (defroutes app-routes
@@ -12,17 +13,18 @@
   ;; 위의 베이스 라우팅 처리시 string을 넣어도 작동하는것은 ring이 알아서 표준 응답으로 바꿔주기 때문이다
   ;; {:status 200 :headers {"ContentType" "text/html; charset=uyf-8"} :body "Hello world"}
   (GET "/cheshire-cat" [] 
-    {:status 200
-     :headers {"Content-Type" "application/json; charset=utf-8"}
-     :body (json/generate-string {:name "Cheshire Cat" :status "grinning"})})
+    (rr/response {:name "Cheshire Cat" :status "grinning!"}))
   ;; route/not-found는 기본 404에러를 만든다
   (route/not-found "Not Found"))
 
 ;; 이 부분에서 우리가 만든 라우트에 다른 기본적인 미들웨어를 추가하기위해, wrap-defaults를 이용한다.
 ;; wrap-defaults 는 어떤 미들웨어들로 구성할지 결정하는 옵션을 받는다. -> 이 옵션들은 Ring-Defaults 라이브러리에서 찾을 수 있다.
 (def app
-  (wrap-defaults app-routes site-defaults))
-
+  (-> app-routes 
+      (ring-json/wrap-json-response)
+      (wrap-defaults site-defaults)))
+;; 위는 app-routes를 받아서 site-default로 래핑하고 다시 그것을 자동화된 json 응답으로 래핑한다.
+;; json 래핑은 체셔 라이브러리를 이용해서 응답의 본문에 있는 모든 컬렉션들을 자동으로 json으로 변환한다.
 
 ;; ## cheshire 설명
 
