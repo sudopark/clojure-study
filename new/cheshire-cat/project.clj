edb4a69c03f54eadabd94f107e496b9fdf6f3d73
sudo.park <sudo.park@kakao.com>
2022-12-16 00:47:09 +0900

    cheshire 라이브러리 추가 및 json으로 응답하기

--- new/cheshire-cat/project.clj
+++ new/cheshire-cat/project.clj
@@ -4,8 +4,11 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [cheshire "5.11.0"]]
+  ;; lein-ring 플러그인은 웹 서버를 시작하는 것과 같은 일반적인 링 작업을 자동화 한다.
   :plugins [[lein-ring "0.12.5"]]
+  ;; :ring :handler 키는 웹 서버를 시작할 때 어디서 app 라우트를 찾아야 하는지 알려준다.
   :ring {:handler cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- new/cheshire-cat/src/cheshire_cat/handler.clj
+++ new/cheshire-cat/src/cheshire_cat/handler.clj
@@ -1,13 +1,20 @@
 (ns cheshire-cat.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [cheshire.core :as json]))
 
 ;; defroutes는 app-routes라는 HTTP 라우트를 만든다. -> 클라에서 요청하는 URL의 경로를 처리함
-(defroutes app-routes 
+(defroutes app-routes
   ;; GET은 HTTP get 라우트를 만든다 -> 여기서는 베이스 url이 요청된 경우 루트 경로를 처리 => "Hello World" 문자열을 출력
   (GET "/" [] "Hello World")
-  (GET "/cheshire-cat" [] "Smile")
+  ;;  ring은 요청과 응답을 모두 map으로 처리한다. -> 그래서 핸들러 함수에서 HTTP 응답을 맵의 형식으로 반환해야 한다.
+  ;; 위의 베이스 라우팅 처리시 string을 넣어도 작동하는것은 ring이 알아서 표준 응답으로 바꿔주기 때문이다
+  ;; {:status 200 :headers {"ContentType" "text/html; charset=uyf-8"} :body "Hello world"}
+  (GET "/cheshire-cat" [] 
+    {:status 200
+     :headers {"Content-Type" "application/json; charset=utf-8"}
+     :body (json/generate-string {:name "Cheshire Cat" :status "grinning"})})
   ;; route/not-found는 기본 404에러를 만든다
   (route/not-found "Not Found"))
 
@@ -15,3 +22,14 @@
 ;; wrap-defaults 는 어떤 미들웨어들로 구성할지 결정하는 옵션을 받는다. -> 이 옵션들은 Ring-Defaults 라이브러리에서 찾을 수 있다.
 (def app
   (wrap-defaults app-routes site-defaults))
+
+
+;; ## cheshire 설명
+
+;; json으로 인코딩: generate-string
+;; (json/generate-string {:name "c" :state :grinning})
+
+;; json으로 부터 디코딩: parse-string -> 기본적으로 키가 문자열인 맵을 반환함
+;; (json/parse-string "{\"name\":\"c\"}") => {"name" "c"}
+;; 두번째 인수를 true로 하면 parse-string은 키가 키워드인 맵을 반환한다.
+;; (json/parse-string "{\"name\":\"c\"}" true) => {:name "c"}
