b94e26ac1d00841f0ae2695ae2e44a289d86350c
sudo.park <sudo.park@kakao.com>
2022-12-29 17:53:30 +0900

    39번까지 진행

--- hello/src/hello/practice.clj
+++ hello/src/hello/practice.clj
@@ -77,3 +77,18 @@ reduce (fn [acc ele] (if (= (last acc) ele) acc (conj acc ele))) []
 
 (fn [start end] (take (- end start) (iterate inc start)))
 
+;; problem 38 - Maximum value
+
+;; (= (__ 1 8 3 4) 8)
+;; (= (__ 30 20) 30)
+;; (= (__ 45 67 11) 67)
+
+(fn [ & xs] (reduce #(if (< %1 %2) %2 %1) xs))
+
+
+;; problem 39 - Interleave Two Seqs
+
+;; (= (__ [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
+;; (= (__ [1 2] [3 4 5 6]) '(1 3 2 4))
+;; (= (__ [1 2 3 4] [5]) [1 5])
+;; (= (__ [30 20] [25 15]) [30 25 20 15])
